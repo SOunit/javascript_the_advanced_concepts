6f056d72550fb8f3f9ca0685c5679950afc9e11e
SOunit <workbookwork7@gmail.com>
2022-02-10 18:11:26 -0800

    delete dist folder

--- 02_foundation/dist/app.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-console.log("hello");
-const test = "test";
-const button = document.querySelector("button");
-button.addEventListener("click", () => {
-    console.log("Clicked");
-});
